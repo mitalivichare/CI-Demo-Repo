c2016e7d424ad27c7fff6a0ddaac302e47681495
Mitali Vichare <vicharemitali26@gmail.com>
2017-07-29 11:02:38 +0530

    commit made successfully to branch

---
+++ DemoProject/src/main/java/com/demo/demoproject/App.java
@@ -0,0 +1,18 @@
+package com.demo.demoproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        App app=new App();
+    }
+    
+    public int add(int n1,int n2)
+    {
+    	return n1+n2;
+    }
+}
